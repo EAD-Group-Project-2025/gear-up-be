5689c2bc2716cb984559b77da19d5de27b63e523
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-12 23:58:09 +0530

    feat: create swagger config for api documentation

---
+++ src/main/java/com/ead/gearup/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.ead.gearup.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "GearUp REST API", version = "1.0", description = "API documentation for GearUp"), security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(name = "bearerAuth", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT")
+public class SwaggerConfig {
+
+}
