44ccaf05f0684c8903aa8de3363c4695857f7019
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-12 23:58:56 +0530

    feat: add health check controller

---
+++ src/main/java/com/ead/gearup/controller/HealthCheckController.java
@@ -0,0 +1,21 @@
+package com.ead.gearup.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+@RestController
+@RequestMapping("/api/public/v1/health-check")
+@SecurityRequirement(name = "bearerAuth")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        String message = "voluntrix-backend is running successfully";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+}
