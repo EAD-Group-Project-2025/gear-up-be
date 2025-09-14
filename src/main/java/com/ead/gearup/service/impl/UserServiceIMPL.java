1b53d7d43f637977ee5267170fe3b0357f1765ed
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-14 20:07:06 +0530

    feat: create user service implmentation layer

---
+++ src/main/java/com/ead/gearup/service/impl/UserServiceIMPL.java
@@ -0,0 +1,56 @@
+package com.ead.gearup.service.impl;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.ead.gearup.dto.user.UserCreateDTO;
+import com.ead.gearup.dto.user.UserLoginDTO;
+import com.ead.gearup.model.User;
+import com.ead.gearup.repository.UserRepository;
+import com.ead.gearup.service.UserService;
+import com.ead.gearup.service.auth.JwtService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceIMPL implements UserService {
+
+    private final PasswordEncoder encoder;
+    private final UserRepository userRepository;
+    private final AuthenticationManager authManager;
+    private final JwtService jwtService;
+
+    @Override
+    public String createUser(UserCreateDTO userCreateDTO) {
+        // Instantiate User entity
+        User user = new User();
+
+        user.setEmail(userCreateDTO.getEmail());
+        user.setName(userCreateDTO.getName());
+        user.setPassword(encoder.encode(userCreateDTO.getPassword()));
+
+        userRepository.save(user);
+
+        return "User created successfully!";
+    }
+
+    public String verifyUser(UserLoginDTO userLoginDTO) {
+        Authentication authentication = authManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(userLoginDTO.getEmail(), userLoginDTO.getPassword()));
+
+        if (!authentication.isAuthenticated()) {
+            throw new BadCredentialsException("invalid creadintials!");
+        }
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        return jwtService.generateAccessToken(userDetails);
+    }
+}
