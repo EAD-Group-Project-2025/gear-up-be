7fbb2c164b0d8a4caffd97156bf80a41cbe81367
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-14 18:12:51 +0530

    feat: enahnced user model

--- src/main/java/com/ead/gearup/model/User.java
+++ src/main/java/com/ead/gearup/model/User.java
@@ -1,26 +1,51 @@
 package com.ead.gearup.model;
 
+import com.ead.gearup.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users") // user is a reserved keyword in SQL, so we use users
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String name;
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @JsonIgnore
     private String password;
-    private String role;
+
+    @Column(name = "email_verified", nullable = false)
+    @Builder.Default
+    private Boolean isVerified = false;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private UserRole role = UserRole.PUBLIC; // default role
+
 }
