f6c13fa1fdc479d5bdc196caceab915867e1373b
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-14 20:08:29 +0530

    chore: change endpints

--- src/main/java/com/ead/gearup/controller/UserController.java
+++ src/main/java/com/ead/gearup/controller/UserController.java
@@ -2,17 +2,12 @@ package com.ead.gearup.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ead.gearup.dto.auth.AuthRequest;
 import com.ead.gearup.dto.user.UserCreateDTO;
-import com.ead.gearup.service.JWTService;
+import com.ead.gearup.dto.user.UserLoginDTO;
 import com.ead.gearup.service.UserService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -24,24 +19,14 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
     private final UserService userService;
-    private final AuthenticationManager authenticationManager;
-    private final JWTService jwtService;
 
-    @PostMapping("/create-user")
-    public ResponseEntity<UserCreateDTO> createUser(@RequestBody UserCreateDTO userCreateDTO) {
+    @PostMapping("/register")
+    public ResponseEntity<String> createUser(@RequestBody UserCreateDTO userCreateDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userCreateDTO));
     }
 
-    @PostMapping("/generate-token")
-    public ResponseEntity<String> getToken(@RequestBody AuthRequest authRequest) {
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
-
-        if (!authentication.isAuthenticated()) {
-            throw new UsernameNotFoundException("Invalid user request!");
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body(jwtService.generateToken(authRequest.getUsername()));
+    @PostMapping("/login")
+    public ResponseEntity<String> getToken(@RequestBody UserLoginDTO userLoginDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.verifyUser(userLoginDTO));
     }
 }
