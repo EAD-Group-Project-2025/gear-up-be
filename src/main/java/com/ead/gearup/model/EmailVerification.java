0eec8cdd65b2ffdf7dc743f0892ae83c93fc710a
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-18 18:15:15 +0530

    feat: create emailverification table

---
+++ src/main/java/com/ead/gearup/model/EmailVerification.java
@@ -0,0 +1,52 @@
+package com.ead.gearup.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "email_verification")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmailVerification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 50)
+    private String otp;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(name = "verified_at")
+    private LocalDateTime verifiedAt;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean isUsed = false;
+}
