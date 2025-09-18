d65f0f0e5ed653acafb03783f334d03412655a79
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-18 18:15:49 +0530

    feat: add email verification repo

--- src/main/java/com/ead/gearup/repository/UserRepository.java
+++ src/main/java/com/ead/gearup/repository/UserRepository.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Repository;
 import com.ead.gearup.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-
-    Optional<User> findByEmailVerificationToken(String token);
 }
