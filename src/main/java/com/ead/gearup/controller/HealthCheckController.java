1aa5d8466d977ede45881e6fc7d2578a1d97a634
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-19 09:05:43 +0530

    feat: add role based authorization for testing purpose

--- src/main/java/com/ead/gearup/controller/HealthCheckController.java
+++ src/main/java/com/ead/gearup/controller/HealthCheckController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ead.gearup.enums.UserRole;
+import com.ead.gearup.validation.RequiresRole;
+
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
@@ -14,6 +17,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 public class HealthCheckController {
 
     @GetMapping
+    @RequiresRole({ UserRole.CUSTOMER })
     public ResponseEntity<String> healthCheck() {
         String message = "voluntrix-backend is running successfully";
         return ResponseEntity.status(HttpStatus.OK).body(message);
