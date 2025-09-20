8917ecedf14bfbfa143f8acc109fa62b71f4a754
vihangarasanjali <rvihanga0506@gmail.com>
2025-09-20 12:23:25 +0530

    Dependency test

--- src/main/java/com/ead/gearup/GearupApplication.java
+++ src/main/java/com/ead/gearup/GearupApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GearupApplication {
 
 	public static void main(String[] args) {
+		System.out.println("URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+		System.out.println("User: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+
 		SpringApplication.run(GearupApplication.class, args);
+
 	}
 
 }
