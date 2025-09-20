59da2dc1ad74f60a23ee56aff2f74a9f70734895
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-20 15:00:49 +0530

    chore: remove printf

--- src/main/java/com/ead/gearup/GearupApplication.java
+++ src/main/java/com/ead/gearup/GearupApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GearupApplication {
 
 	public static void main(String[] args) {
-		System.out.println("URL: " + System.getenv("SPRING_DATASOURCE_URL"));
-		System.out.println("User: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
 
 		SpringApplication.run(GearupApplication.class, args);
 
