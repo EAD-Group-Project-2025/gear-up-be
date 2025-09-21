ca3989d21a3d6ce030dbccabfbc7ca6b986f0b99
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-21 12:26:12 +0530

    feat: validate phone number

--- src/main/java/com/ead/gearup/dto/customer/CustomerUpdateDTO.java
+++ src/main/java/com/ead/gearup/dto/customer/CustomerUpdateDTO.java
@@ -2,6 +2,7 @@ package com.ead.gearup.dto.customer;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CustomerRequestDTO {
+public class CustomerUpdateDTO {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
 
     @NotBlank(message = "Phone number is required")
     @Pattern(regexp = "^(?:0|\\+94)?7[0-9]{8}$", message = "Phone number must be a valid Sri Lankan number")
     private String phoneNumber;
+
 }
