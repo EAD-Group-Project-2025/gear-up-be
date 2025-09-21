720c9c364f73e897de6e9a4f4eb6cb70dbda01d6
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-21 11:28:55 +0530

    chore: add name

--- src/main/java/com/ead/gearup/dto/customer/CustomerResponseDTO.java
+++ src/main/java/com/ead/gearup/dto/customer/CustomerResponseDTO.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerResponseDTO {
-    private Long customerId;
+    private String name;
     private String email;
     private String phoneNumber;
 }
