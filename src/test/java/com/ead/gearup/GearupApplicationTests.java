41ace80387b8581e7965f4dfecb5b166828db816
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-21 19:51:31 +0530

    feat: setup for test

--- src/test/java/com/ead/gearup/GearupApplicationTests.java
+++ src/test/java/com/ead/gearup/GearupApplicationTests.java
@@ -2,8 +2,10 @@ package com.ead.gearup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GearupApplicationTests {
 
 	@Test
