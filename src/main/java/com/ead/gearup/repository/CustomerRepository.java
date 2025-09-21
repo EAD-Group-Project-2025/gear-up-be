34326a709ff1541597b4dfe008a496f32d71c841
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-21 11:29:57 +0530

    feat: add repository annotation

--- src/main/java/com/ead/gearup/repository/CustomerRepository.java
+++ src/main/java/com/ead/gearup/repository/CustomerRepository.java
@@ -2,9 +2,11 @@ package com.ead.gearup.repository;
 
 import com.ead.gearup.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByUserEmail(String email);
-
 }
