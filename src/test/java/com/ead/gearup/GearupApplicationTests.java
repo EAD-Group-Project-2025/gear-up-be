c4f3208c491440d80cf2caedb204008d8e06cd7c
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-21 20:11:43 +0530

    chore: update

--- src/test/java/com/ead/gearup/GearupApplicationTests.java
+++ src/test/java/com/ead/gearup/GearupApplicationTests.java
@@ -1,12 +1,38 @@
 package com.ead.gearup;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.ead.gearup.repository.UserRepository;
+import com.ead.gearup.service.EmailVerificationService;
+import com.ead.gearup.service.auth.CustomUserDetailsService;
+import com.ead.gearup.service.auth.JwtService;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("test") // uses application-test.properties
 class GearupApplicationTests {
+
+	@Mock
+	private EmailVerificationService emailVerificationService;
+
+	@Mock
+	private JwtService jwtService;
+
+	@Mock
+	private CustomUserDetailsService customUserDetailsService;
+
+	@Mock
+	private AuthenticationManager authManager;
+
+	@Mock
+	private PasswordEncoder passwordEncoder;
+
+	@Mock
+	private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
