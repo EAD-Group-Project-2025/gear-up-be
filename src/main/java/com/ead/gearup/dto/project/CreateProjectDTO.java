f10a7b685067f455660f592b0814af52c5eb7834
vihangarasanjali <rvihanga0506@gmail.com>
2025-09-29 05:01:07 +0530

    Feat(Project): Add Project model

--- src/main/java/com/ead/gearup/dto/project/CreateProjectDTO.java
+++ src/main/java/com/ead/gearup/dto/project/CreateProjectDTO.java
@@ -1,6 +1,5 @@
 package com.ead.gearup.dto.project;
 
-import com.ead.gearup.model.Task;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
