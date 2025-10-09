84276ce74f96b828206355234cce2560dba5fd17
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-10-09 17:07:10 +0530

    feat: add search jpql query

--- src/main/java/com/ead/gearup/repository/AppointmentRepository.java
+++ src/main/java/com/ead/gearup/repository/AppointmentRepository.java
@@ -1,11 +1,22 @@
 package com.ead.gearup.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.ead.gearup.model.Appointment;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.ead.gearup.model.Appointment;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    @Query("SELECT a FROM Appointment a " +
+            "JOIN FETCH a.customer c " +
+            "JOIN FETCH c.user u " +
+            "WHERE LOWER(u.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<Appointment> findByCustomerUserName(@Param("name") String name);
 
 }
