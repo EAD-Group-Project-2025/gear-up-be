f08ac2dc7c8c0d58640dba17450e69311a9778ac
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-10-11 23:16:23 +0530

    feat: add method to count projects by status for employee

--- src/main/java/com/ead/gearup/repository/ProjectRepository.java
+++ src/main/java/com/ead/gearup/repository/ProjectRepository.java
@@ -1,10 +1,22 @@
 package com.ead.gearup.repository;
 
 import com.ead.gearup.model.Project;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    @Query("SELECT p.status, COUNT(p) " +
+           "FROM Project p " +
+           "JOIN p.assignedEmployees e " +
+           "WHERE e.employeeId = :employeeId " +
+           "GROUP BY p.status")
+    List<Object[]> countProjectsByStatusForEmployee(@Param("employeeId") Long employeeId);
 
 }
